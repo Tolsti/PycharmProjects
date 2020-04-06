ee185ac6331b49c6d71df6590860d353cd62fa90
Tolsti <tolsti_web@mail.ru>
2020-04-06 13:59:07 +0300

    +

--- Sort/BubbleSort.py
+++ Sort/BubbleSort.py
@@ -1,9 +1,17 @@
 import random
 
-arr = [random.randint(1, 100) for i in range(1, 10)]
-print(arr)
+array = [random.randint(1, 100) for i in range(10)]
 
 
 def bubble_sort(arr):
-    while True:
-        pass
+    for lap in range(len(arr) - 1):
+        for i in range(len(arr) - 1 - lap):
+            if arr[i] > arr[i + 1]:
+                tmp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = tmp
+    return arr
+
+
+print(array)
+print(bubble_sort(array))
